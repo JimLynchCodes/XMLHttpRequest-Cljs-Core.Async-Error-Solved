72b764b9b3eeb205943969dd50cd85ffd8e1234e
JimTheMan <mrdotjim@gmail.com>
2020-01-11 16:02:54 -0500

    WIP - add example function

--- src/main/clojurescript/serverless/functions.cljs
+++ src/main/clojurescript/serverless/functions.cljs
@@ -7,18 +7,30 @@
 (defn hello [event ctx cb]
   (println ctx)
   (cb nil (clj->js
-            {:statusCode 200
-             :headers    {"Content-Type" "text/html"}
-             :body       "<h1>Hello, World!</h1>"})))
+           {:statusCode 200
+            :headers    {"Content-Type" "text/html"}
+            :body       "<h1>Hello, World!</h1>"})))
 
 
 (defn now [event ctx cb]
   (println ctx)
   (cb nil (clj->js
-            {:statusCode 200
-             :headers    {"Content-Type" "text/html"}
-             :body       (str "<h1>"(.format (moment.) "LLLL")"</h1>")}))) ; call nodejs package
+           {:statusCode 200
+            :headers    {"Content-Type" "text/html"}
+            :body       (str "<h1>" (.format (moment.) "LLLL") "</h1>")}))) ; call nodejs package
+
+(defn example [event ctx cb]
+  (go (let [response (<! (http/get "https://sv443.net/jokeapi/category/Programming"))
+            theGloriousData  (->> response
+                                  (:body)
+                                  (clj->js)
+                                  (.stringify js/JSON))]
+        (cb nil (clj->js
+                 {:statusCode 200
+                  :headers    {"Content-Type" "application/json"}
+                  :body       theGloriousData})))))
 
 (set! (.-exports js/module) #js
-    {:hello hello
-     :now now})
+                             {:hello hello
+                              :now now
+                              :example example})
